7180173a514a7e408c8df24a0b715bfcf3bab83a
Yosbel <yosbelmc22@gmail.com>
2024-06-10 13:57:37 -0400

    header and some others stuff

---
+++ src/header.jsx
@@ -0,0 +1,18 @@
+export const Header = () => {
+    return (
+         <div className="body">
+            <div className="nav">
+                <div><a href="" className="atrib">Proyectos</a></div>
+                <div><a href="" className="atrib">Skills</a></div>
+                <div><a href="" className="atrib">About me</a></div>
+                <div><a href="" className="atrib">Contact</a></div>
+            </div>
+            <div className="texto">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium nemo alias exercitationem, eligendi, tempore, vero dignissimos recusandae expedita ad quisquam veniam itaque quae rem quod eius labore? Ut, molestiae accusantium!</p>
+            </div>
+            
+         </div>
+    
+    )
+}
+       
