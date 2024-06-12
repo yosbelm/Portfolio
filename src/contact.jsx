892872252dd714b1f93717e28c8bfe161f4afe4c
Yosbel <yosbelmc22@gmail.com>
2024-06-12 18:16:39 -0400

    about me section

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,5 +1,6 @@
 import ContactP from './assets/contact-logo.png'
 import React from 'react';
+import Copy from './assets/copy.png'
 
 export const ContactMe = () => {
     const email = "yosbelm99@gmail.com";
@@ -22,17 +23,36 @@ export const ContactMe = () => {
             </div>
             <div>
                 <h3 className='get-touch-text'>Get in touch</h3>
-                <h3 className='contact-me-text'>Contact me</h3>
+                {/* <h3 className='contact-me-text'>Contact me</h3> */}
             </div>
             <div className="email-container">
                 <div className='mail-exterior'>
                     <span className='email-text'>{email}</span>
                     <button className='button-copy-mail' onClick={copiarAlPortapapeles}>
-                        <img src={ContactP} alt="" /> {/* Icono de copia */}
+                        <img src={Copy} alt="" />
                     </button>
                 </div>
-                
             </div>
+            <form action="" className='mail-container'>
+                <div className='email-field-not-button'>
+                    <div className='email-field-text-div'>
+                        <label htmlFor="name" hidden>Name</label>
+                        <input className='email-field-text' type="text" placeholder='Name' required/>
+                    </div>
+                    <div className='email-field-email-div'>
+                        <label htmlFor="email" hidden>Email</label>
+                        <input className='email-field-email' type="email" placeholder='Email' required/>
+                    </div>
+                    <div className='email-field-message-div'>
+                        <label htmlFor="message" hidden>Message</label>
+                        <textarea className='email-field-message' type="text" placeholder='Message' required></textarea>
+                    </div>
+                </div>
+                <div className='email-submit'>
+                    <input className='email-submit-button' type="submit" placeholder='Submit'/>
+                </div>
+            </form>
+                
 
         </section>
     )
