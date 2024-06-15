1695d81728fb39871e7884a9ff7ec8dff098e60a
Yosbel <yosbelmc22@gmail.com>
2024-06-15 00:09:07 -0400

    light

--- src/common/ThemeContext.jsx
+++ src/common/ThemeContext.jsx
@@ -4,7 +4,7 @@ import {createContext, useState} from "react";
 export const ThemeContext = createContext(null);
 
 export const ThemeProvider = ({children}) => {
-    const [theme, setTheme] = useState("dark");
+    const [theme, setTheme] = useState("light");
     const toggleTheme = () => {
         setTheme((curr) => (curr === 'light'? 'dark': 'light'));
     };
