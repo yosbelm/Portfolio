3a3bd6e68bbdd92393fc9fbace8c7fefd876fcc0
Yosbel <yosbelmc22@gmail.com>
2024-07-10 16:00:48 -0400

    added my Resume

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -11,7 +11,7 @@ export const Header = () => {
     return (
         <div className={styles.headernav}>
             <div id= "#nav" className={styles.nav}>
-                <div><a href="#proyects" className={styles.atrib}>Proyects</a></div>
+                <div><a href="#proyects" className={styles.atrib}>Projects</a></div>
                 <div ><a href="#skills" className={styles.atrib}>Skills</a></div>
                 <div><a href="#about" className={styles.atrib}>About me</a></div>
                 <div><a href="#contact" className={styles.atrib}>Contact</a></div>
--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import Profile from "../../src/assets/hero-banner.jpg"
 import GithubWhite from "../../src/assets/github.png"
 import GitHubDark from "../../src/assets/github-dark.svg"
+import cvFile from "../../src/assets/Resume.pdf";
 import Linkedin from "../../src/assets/linkedinblue.png"
 import styles from './HeroStyles.module.css' 
 import { useTheme } from "../common/ThemeContext"
@@ -8,7 +9,16 @@ import { useTheme } from "../common/ThemeContext"
 export const Hero = () => {
         const { theme } = useTheme();
         const githubIconSrc = theme === 'dark' ? GitHubDark : GithubWhite;
-    
+
+        
+        const handleDownload = () => {
+            const link = document.createElement('a');
+            link.href = cvFile;
+            link.download = 'Resume.pdf';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        };
     return (
         <section id="hero" className={styles.container}>
             <div className={styles.profilePicDiv}>
@@ -21,7 +31,7 @@ export const Hero = () => {
                     <p className={styles.frontend}>FullStack Developer</p>
                 </div>
                 <div className={styles.botonContainer}>
-                    <button className={styles.botonCv} onClick="">Download CV</button>
+                    <button className={styles.botonCv} onClick={handleDownload}>Download CV</button>
                     <button className={styles.botonContact} onClick={() => location.href='mailto:yosbelm99@gmail.com'}>Email</button>
                 </div>
                 <div className={styles.socialMedia}>
@@ -36,4 +46,4 @@ export const Hero = () => {
         
            
     )
-} 
+}
